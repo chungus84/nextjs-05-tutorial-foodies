d4d628ba093671c44ed026bfd6bd06dd42b8e47f
kenichi beveridge <kenbev84@gmail.com>
2024-06-24 22:00:41 +0100

    Initial tutorial project setup

---
+++ app/meals/error.js
@@ -0,0 +1,7 @@
+'use client'
+export default function Error() {
+    return <main className="error">
+        <h1>An error occurred!</h1>
+        <p>failed to fetch meal data. Please try again later!</p>
+    </main>
+}
